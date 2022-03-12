df03acf71218175905fdcbcfa66f647c883dbe67
Ben Rogers <benrogers1299@outlook.com>
2022-03-12 15:20:22 +1000

    Init

---
+++ resources/views/post.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="styles.css">
+
+<body>
+<article>
+    <h1><a href="/post">My First Post</a></h1>
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aliquam animi aperiam atque commodi
+        consequatur enim in inventore ipsum, nesciunt nobis pariatur placeat quas quibusdam quis quo sed sit unde velit
+        voluptates. Delectus deserunt, eveniet iusto laudantium suscipit tempore. Ab accusamus adipisci alias amet
+        aperiam commodi ea eaque earum eligendi et excepturi exercitationem expedita illum laborum modi molestias
+        necessitatibus non omnis pariatur, quae quaerat quas quisquam sed soluta vel voluptas voluptatibus. Commodi
+        explicabo ipsum libero nihil nostrum suscipit voluptas. Blanditiis deleniti distinctio pariatur quis
+        repudiandae. Consequatur culpa doloremque eum explicabo, facilis, harum impedit omnis provident quaerat, quos
+        rem repellendus veritatis.</p>
+</article>
+
+<a href="/">Go Back</a>
+</body>
