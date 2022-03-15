b1aa5126773452cd26de05f8b37904685d649bb7
Ben Rogers <47819009+ben04rogers@users.noreply.github.com>
2022-03-15 20:55:20 +1000

    Add filesystem stuff

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,22 +15,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('posts');
+    $posts = Post::all();
+
+    dd($posts);
+
+    return view('posts', [
+        'posts' => $posts
+    ]);
 });
 
 Route::get('posts/{post}', function ($slug) {
-    $path = __DIR__ . "/../resources/posts/{$slug}.html";
-
-    // Redirect to home page if file doesn't exist
-    if (!file_exists($path)) {
-        return redirect('/');
-    }
-
-    $post = cache()->remember("posts.{$slug}", 1200, function() use ($path) {
-        return file_get_contents($path);
-    });
-
     return view('post', [
-        'post' => $post
+        'post' => Post::find($slug),
     ]);
 })->where('post', '[A-z_\-]+');
