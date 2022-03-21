797e301b881a7b7528fda7ab7dd98e8487ac94f0
Ben Rogers <47819009+ben04rogers@users.noreply.github.com>
2022-03-21 21:13:39 +1000

    Setup database factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,44 +17,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // Clear tables
-        User::truncate();
-        Category::truncate();
-        Post::truncate();
-
-         $user = User::factory()->create();
-
-         $personal = Category::create([
-             'name' => 'Personal',
-             'slug' => 'personal'
-         ]);
-
-        $family = Category::create([
-            'name' => 'Family',
-            'slug' => 'family'
-        ]);
-
-        $work = Category::create([
-            'name' => 'Work',
-            'slug' => 'work'
-        ]);
-
-        Post::create([
-            'title' => 'My family post',
-            'body' => '<p>This is my family post</p>',
-            'user_id' => $user->id,
-            'category_id' => $family->id,
-            'slug' => 'my-family-post',
-            'excerpt' => '<p>This is my family post</p>'
-        ]);
-
-        Post::create([
-            'title' => 'My Work post',
-            'body' => '<p>This is my work post</p>',
-            'user_id' => $user->id,
-            'category_id' => $work->id,
-            'slug' => 'my-work-post',
-            'excerpt' => '<p>This is my work post</p>'
-        ]);
+        Post::factory(5)->create();
     }
 }
