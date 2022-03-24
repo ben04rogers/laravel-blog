c88a495734e8d51bb7bb833aeb3ce535be760631
Ben Rogers <47819009+ben04rogers@users.noreply.github.com>
2022-03-24 19:02:58 +1000

    Minor change

--- resources/views/components/post-featured-card.blade.php
+++ resources/views/components/post-featured-card.blade.php
@@ -1,8 +1,9 @@
+@props(['post'])
 <article
     class="transition-colors duration-300 hover:bg-gray-100 border border-black border-opacity-0 hover:border-opacity-5 rounded-xl">
     <div class="py-6 px-5 lg:flex">
         <div class="flex-1 lg:mr-8">
-            <img src="./images/illustration-1.png" alt="Blog Post illustration" class="rounded-xl">
+            <img src="/images/illustration-1.png" alt="Blog Post illustration" class="rounded-xl">
         </div>
 
         <div class="flex-1 flex flex-col justify-between">
@@ -19,7 +20,7 @@
 
                 <div class="mt-4">
                     <h1 class="text-3xl">
-                        This is a big title and it will look great on two or even three lines. Wooohoo!
+                        {{ $post->title }}
                     </h1>
 
                     <span class="mt-2 block text-gray-400 text-xs">
