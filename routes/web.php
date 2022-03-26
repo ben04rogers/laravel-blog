e1df1454042369d7d92b53548acfecafa44beb83
Ben Rogers <47819009+ben04rogers@users.noreply.github.com>
2022-03-26 15:20:58 +1000

    Add category dropdowm

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,7 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 Route::get('/', function () {
     return view('posts', [
         'posts' => Post::latest()->get(),
+        'categories' => Category::all(),
     ]);
 });
 
@@ -31,7 +32,9 @@ Route::get('posts/{post:slug}', function (Post $post) {
 
 Route::get('categories/{category:slug}', function (Category $category) {
     return view('posts', [
-        'posts' => $category->posts
+        'posts' => $category->posts,
+        'currentCategory' => $category,
+        'categories' => Category::all()
     ]);
 });
 
