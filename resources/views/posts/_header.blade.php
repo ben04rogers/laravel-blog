ff3fd1e4ec0bdff48a98dea09b323d2402ecbdcf
Ben Rogers <benrogers1299@outlook.com>
2022-03-29 22:01:53 +1000

    Merge search and category queries

--- resources/views/posts/_header.blade.php
+++ resources/views/posts/_header.blade.php
@@ -22,7 +22,7 @@
 
 {{--            <svg class="transform -rotate-90 absolute pointer-events-none" style="right: 12px;" width="22"--}}
 {{--                 height="22" viewBox="0 0 22 22">--}}
-{{--                <g fill="none" fill-rule="evenodd">--}} 
+{{--                <g fill="none" fill-rule="evenodd">--}}
 {{--                    <path stroke="#000" stroke-opacity=".012" stroke-width=".5" d="M21 1v20.16H.84V1z">--}}
 {{--                    </path>--}}
 {{--                    <path fill="#222"--}}
@@ -34,6 +34,10 @@
         <!-- Search -->
         <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl px-3 py-2">
             <form method="GET" action="#">
+                @if (request('category'))
+                    <input type="hidden" name="category" value="{{ request('category') }}">
+                @endif
+
                 <input type="text" name="search" placeholder="Find something"
                        class="bg-transparent placeholder-black font-semibold text-sm"
                        value="{{ request('search') }}">
